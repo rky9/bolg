359052969114c6538c777029e94c6c1f84cc63cd
rajesh <rajesh.coer@gmail.com>
2022-12-03 18:36:33 +0530

    nevbar added

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,6 +1,18 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Toolbar, styled, Typography } from "@mui/material";
+import Pets from "@mui/icons-material/Pets";
 import React from "react";
 
+const StyleTdToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
 export const Navbar = () => {
-  return <AppBar>Navbar</AppBar>;
+  return (
+    <AppBar position="sticky">
+      <StyleTdToolbar>
+        <Typography variant="h6">LAMA DEV</Typography>
+        <Pets />
+      </StyleTdToolbar>
+    </AppBar>
+  );
 };
