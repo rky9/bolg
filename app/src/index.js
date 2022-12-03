4eca27eb59c91d37fb52b2c296a1bf63627d2df2
rajesh <rajesh.coer@gmail.com>
2022-12-03 14:27:11 +0530

    theme removed

--- app/src/index.js
+++ app/src/index.js
@@ -9,9 +9,7 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
