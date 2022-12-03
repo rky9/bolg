df9d2c6449b3a98d9ad2a7f2874dc9b7b9015583
rajesh <rajesh.coer@gmail.com>
2022-12-03 21:33:52 +0530

    Search bar added to navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,17 +1,45 @@
-import { AppBar, Toolbar, styled, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+} from "@mui/material";
 import Pets from "@mui/icons-material/Pets";
+import Mail from "@mui/icons-material/Mail";
 import React from "react";
 
 const StyleTdToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "#ffffff",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+const Icons = styled(Box)(({ theme }) => ({}));
 export const Navbar = () => {
   return (
     <AppBar position="sticky">
       <StyleTdToolbar>
-        <Typography variant="h6">LAMA DEV</Typography>
-        <Pets />
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          LAMA DEV
+        </Typography>
+
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+        </Icons>
       </StyleTdToolbar>
     </AppBar>
   );
